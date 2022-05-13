a57c88df8f877d96e67767f1442f06b7e2b0cd8e
Deepcong <735005214@qq.com>
2022-05-13 22:52:16 +0800

    Update yolo_slim.py

--- yolo_slim.py
+++ yolo_slim.py
@@ -66,7 +66,7 @@ class YOLO(object):
         #   验证集损失较低不代表mAP较高，仅代表该权值在验证集上泛化性能较好。
         #   如果出现shape不匹配，同时要注意训练时的model_path和classes_path参数的修改
         #--------------------------------------------------------------------------#
-        "model_path"        : 'model_data/foda_cargo_dn.h5',
+        "model_path"        : 'model_data/yolov4.h5',
         "classes_path"      : 'model_data/my_classes.txt',
         #---------------------------------------------------------------------#
         #   anchors_path代表先验框对应的txt文件，一般不修改。
